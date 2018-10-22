a4ec4c2b544ce255fea96f490943b0b75528635e
YukaSan <mr.uakor@gmail.com>
2018-10-22 21:47:42 +0300

    home

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ComponentRef } from '@angular/core';
 import { OpenStreetMapProvider } from 'leaflet-geosearch';
-import {icon, latLng, marker, tileLayer, map} from 'leaflet';
+import {icon, latLng, marker, tileLayer, Map} from 'leaflet';
 
-
+declare let L;
 
 @Component({
   selector: 'app-home-page',
@@ -11,50 +11,46 @@ import {icon, latLng, marker, tileLayer, map} from 'leaflet';
 })
 export class HomePageComponent implements OnInit {
 
+  public map: Map;
 
   options = {
     layers: [
-      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 18, attribution: '...'})
     ],
     zoom: 5,
     center: latLng(46.879966, 21.726909)
   };
 
   layers = [
-    marker([ 43.879966, 21.726909 ] , {
+    marker([43.879966, 21.726909], {
       icon: icon({
-        iconSize: [ 25, 31 ],
-        iconAnchor: [ 13, 41 ],
+        iconSize: [25, 31],
+        iconAnchor: [13, 41],
         iconUrl: '/node_modules/leaflet/dist/images/marker-icon.png',
         shadowUrl: '/node_modules/leaflet/dist/images/marker-shadow.png'
       })
     }),
-    marker([ 46.879966, 21.726909 ] , {
+    marker([46.879966, 21.726909], {
       icon: icon({
-        iconSize: [ 25, 31 ],
-        iconAnchor: [ 13, 41 ],
+        iconSize: [25, 31],
+        iconAnchor: [13, 41],
         iconUrl: '/node_modules/leaflet/dist/images/marker-icon.png',
         shadowUrl: '/node_modules/leaflet/dist/images/marker-shadow.png'
       })
     })
   ];
-  constructor() { }
+
+  constructor() {
+  }
 
   ngOnInit() {
-    this.layers.push(marker([ 48.879966, 21.726909 ] , {
-      icon: icon({
-        iconSize: [ 25, 31 ],
-        iconAnchor: [ 13, 41 ],
-        iconUrl: '/node_modules/leaflet/dist/images/marker-icon.png',
-        shadowUrl: '/node_modules/leaflet/dist/images/marker-shadow.png'
-      })
-    }));
+    window.navigator.geolocation.getCurrentPosition((position: Position) => {
 
-
-    this.layers.forEach(function (p) {
-     console.log(p);
-
-
+      this.map = L.map('map').setView([
+        position.coords.latitude,
+        position.coords.longitude
+      ], 13);
     }
-  );
+    );
   }
+}
